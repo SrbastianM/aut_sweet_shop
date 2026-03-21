f17c1511dc6341caac2f450a679e19519861f3fd
Sebastian Rodriguez <srodriguezdev.l@gmail.com>
2026-03-21 18:53:33 -0500

    hfix:

--- utils/envs.ts
+++ utils/envs.ts
@@ -2,7 +2,7 @@ export function getEnv(name: string): string {
     const value = process.env[name];
 
     if (!value) {
-        throw new Error(`Missing env variable:  ${name}`);
+        throw new Error(`Missing env variable:  ${name}. Check .env or Github actions`);
     };
 
     return value;
