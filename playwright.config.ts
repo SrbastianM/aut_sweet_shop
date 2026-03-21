10ffc4731898d6652a42e7f541adff366d5f6501
Sebastian Rodriguez <srodriguezdev.l@gmail.com>
2026-03-21 13:56:42 -0500

    feat: Add allure report to playwright 	- Seting up the allure report to use with node and playwright. 	- Changing the gitignore to ignore the allure reports and results. 	- Adding new dependencies: faker-js, playwright-testing-library/test and allurex

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,47 @@
 import { defineConfig, devices } from '@playwright/test';
+import { Status } from "allure-js-commons";
+import * as os from "node:os";
+
 export default defineConfig({
   testDir: './front/test',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    ["line"],
+    [
+      "allure-playwright", {
+        resultsDir: "allure-results",
+        detail: true,
+        suiteTitle: true,
+        links: {
+          issue: {
+            nameTemplate: "Issue #%s",
+            urlTemplate: "https://issues.example.com/%s",
+          },
+          tms: {
+            nameTemplate: "TMS #%s",
+            urlTemplate: "https://tms.example.com/%s",
+          },
+        },
+        categories: [
+          {
+            name: "foo",
+            messageRegex: "bar",
+            traceRegex: "baz",
+            matchedStatuses: [Status.FAILED, Status.BROKEN],
+          },
+        ],
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+        },
+      },
+    ]
+  ],
   use: {
     trace: 'on-first-retry',
   },
